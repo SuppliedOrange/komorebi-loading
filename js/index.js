8de90d0f5bff0c687070f0c9a648e16ff8bb43e5
Gurunath-S <guruthedev20@example.com>
2025-07-05 23:26:35 +0530

    Feat: Add minimize button and removed the animations to close button

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
 addEventListener("DOMContentLoaded", (event) => {
+	const closeButtons = document.querySelectorAll(".close-button");
 
-    const closeButtons = document.querySelectorAll('.close-button');
-
-    closeButtons.forEach((closeButton) => {
-        
-        closeButton.addEventListener('mouseover', () => {
-            closeButton.textContent = 'goodbye :(';
+	closeButtons.forEach((closeButton) => {
+		closeButton.addEventListener("mouseover", () => {
+            closeButton.textContent = "goodbye :(";
         });
 
-        closeButton.addEventListener('mouseout', () => {
-            closeButton.textContent = 'end';
+        closeButton.addEventListener("mouseout", () => {
+            closeButton.textContent = "end";
         });
 
-        closeButton.addEventListener('click', () => {
-            window.close();
-        });
-
-    });
-
+		closeButton.addEventListener("click", () => {
+			window.close();
+		});
+	});
 });
